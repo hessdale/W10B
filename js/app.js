911a96e4a43e18dcb25e96a83f2899990bcb8e1a
dale <hessdale96@gmail.com>
2023-03-23 21:02:25 -0600

    added variables in js and trying to get loops to change arrays

---
+++ js/app.js
@@ -0,0 +1,25 @@
+//using variables to get ids and all p tags using querySelector
+let get_id_1 = document.querySelector(`#text_1`);
+let all_p_tags = document.querySelectorAll(`p`);
+//using variables and document.getElement to define them
+let class_1 = document.getElementsByClassName(`class_1`);
+let get_id_2 = document.getElementById(`text_2`);
+
+
+//using the variables to change the style of this p-tag
+get_id_1[`style`][`color`] = `green`;
+
+//because this is an array I used a loop to go through and change each
+for (counter = 0; counter < all_p_tags.length; counter++) {
+    all_p_tags[counter][`outerText`] = `<p>CHANGED!</p>`;
+};
+
+//because this is an array I used a loop to go through and change each
+for (counter = 0; counter < class_1.length; counter++) {
+    class_1[counter].insertAdjacentHTML(`afterend`, `<p>new after</p>`);
+};
+
+//using the variables to change the html on index.html
+get_id_2.insertAdjacentHTML(`beforebegin`, `<p>new before</p>`);
+
+
